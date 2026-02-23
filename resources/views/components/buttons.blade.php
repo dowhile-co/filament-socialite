ae6eed43f8898ed8a4da2420764c1ded0a0e9c46
zvizvi <5633910@gmail.com>
2026-02-23 05:45:02 +0200

    Add dynamic divider label based on auth page type and improve styling

--- resources/views/components/buttons.blade.php
+++ resources/views/components/buttons.blade.php
@@ -3,6 +3,11 @@
     x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('filament-socialite-styles', package: 'filament-socialite'))]"
 >
     @php
+        $isRegisterPage = str_contains((string) request()->route()?->getName(), '.auth.register');
+        $dividerLabel = $isRegisterPage
+            ? __('filament-socialite::auth.register-via')
+            : __('filament-socialite::auth.login-via');
+
         $googleIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="18" viewBox="0 0 48 48" aria-hidden="true" focusable="false" style="display: block;">
             <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
             <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
@@ -23,8 +28,8 @@
             @if($showDivider)
                 <div class="relative flex items-center justify-center text-center">
                     <div class="absolute border-t border-gray-200 w-full h-px"></div>
-                    <p class="inline-block relative bg-white text-sm p-2 rounded-full font-medium text-gray-500 dark:bg-gray-800 dark:text-gray-100">
-                        {{ __('filament-socialite::auth.login-via') }}
+                    <p class="inline-block relative bg-white text-sm px-4 p-2 font-medium">
+                        {{ $dividerLabel }}
                     </p>
                 </div>
             @endif
